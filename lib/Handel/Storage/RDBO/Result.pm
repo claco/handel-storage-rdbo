d9a16b22ca0bcb5942f01350b27f05248a055a94
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2006-12-13 03:47:04 +0000

    Added pod
    git-svn-id: http://svn.icantfocus.com/CPAN/Handel-Storage-RDBO/trunk@1619 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Handel/Storage/RDBO/Result.pm
+++ lib/Handel/Storage/RDBO/Result.pm
@@ -30,3 +30,80 @@ sub update {
 
 1;
 __END__
+
+=head1 NAME
+
+Handel::Storage::RDBO::Result - Result object returned by RDBO storage operations
+
+=head1 SYNOPSIS
+
+    use Handel::Storage::RDBO::Cart;
+    
+    my $storage = Handel::Storage::RDBO::Cart->new;
+    my $result = $storage->create({
+        shopper => '11111111-1111-1111-1111-111111111111'
+    });
+    
+    print $result->id;
+    print $result->name;
+
+=head1 DESCRIPTION
+
+Handel::Storage::RDBO::Result is a generic wrapper around RDBO objects returned
+by various Handel::Storage::RDBO operations. Its main purpose is to abstract
+storage result objects away from the Cart/Order/Item classes that use them and
+deal with any RDBO specific issues. Each result is assumed to exposed methods
+for each 'property' or 'column' it has, as well as support the methods
+described below.
+
+=head1 METHODS
+
+=head2 delete
+
+Deletes the current result and all of it's associated items from the current
+storage.
+
+    my $storage = Handel::Storage::RDBO::Cart->new;
+    my $result = $storage->create({
+        shopper => '11111111-1111-1111-1111-111111111111'
+    });
+    
+    $result->add_item({
+        sku => 'ABC123'
+    });
+    
+    $result->delete;
+
+=head2 discard_changes
+
+Discards all changes made since the last successful update.
+
+=head2 update
+
+=over
+
+=item Arguments: \%data
+
+=back
+
+Updates the current result with the data specified.
+
+    my $storage = Handel::Storage::RDBO::Cart->new;
+    my $result = $storage->create({
+        shopper => '11111111-1111-1111-1111-111111111111'
+    });
+    
+    $result->update({
+        name => 'My Cart'
+    });
+
+=head1 SEE ALSO
+
+L<Handel::Storage::Result>, L<Rose::DB::Object>
+
+=head1 AUTHOR
+
+    Christopher H. Laco
+    CPAN ID: CLACO
+    claco@chrislaco.com
+    http://today.icantfocus.com/blog/
